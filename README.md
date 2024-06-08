640abfa450bd18988bc2f09943c3e083bfe35b42
Rub21 <rub2106@gmail.com>
2024-06-08 12:28:16 -0500

    Update config

--- README.md
+++ README.md
@@ -5,8 +5,50 @@
 aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-0463def14f555a7ed" --query "Subnets[*].{ID:SubnetId,CIDR:CidrBlock,AZ:AvailabilityZone,Public:MapPublicIpOnLaunch}"
 
 
-subnet-0f8836c96b7d3305f
-subnet-065d9574978041652
-subnet-0b7faf7a4a01e03ff
-subnet-0920a1dff5c03103b
-subnet-02c7bfb3450e3cb14 
+
+```
+  {
+        "ID": "subnet-0f8836c96b7d3305f",
+        "CIDR": "172.31.0.0/20",
+        "AZ": "us-east-1d",
+        "Public": true
+    },
+    {
+        "ID": "subnet-0920a1dff5c03103b",
+        "CIDR": "172.31.32.0/20",
+        "AZ": "us-east-1c",
+        "Public": true
+    },
+    {
+        "ID": "subnet-049a6317b90706776",
+        "CIDR": "172.31.16.0/20",
+        "AZ": "us-east-1b",
+        "Public": true
+    },
+    {
+        "ID": "subnet-02c7bfb3450e3cb14",
+        "CIDR": "172.31.80.0/20",
+        "AZ": "us-east-1a",
+        "Public": true
+    },
+    {
+        "ID": "subnet-065d9574978041652",
+        "CIDR": "172.31.48.0/20",
+        "AZ": "us-east-1e",
+        "Public": true
+    },
+    {
+        "ID": "subnet-0b7faf7a4a01e03ff",
+        "CIDR": "172.31.64.0/20",
+        "AZ": "us-east-1f",
+        "Public": true
+    },
+```
+
+
+```sh
+aws eks update-kubeconfig --region us-east-1 --name geocompas-k8s-production
+kubectl cluster-info
+```
+
+t3_medium_ondemand
--- deploy.sh
+++ deploy.sh
@@ -1,59 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-function get_az_public_subnet() {
-    local vpc_name="$1"
-    local region="$2"
-    local vpc_id=$(aws ec2 describe-vpcs --region "$region" --filters "Name=tag:Name,Values=$vpc_name" --query 'Vpcs[0].VpcId' --output text)
-    local az_public_subnet=$(aws ec2 describe-subnets --region "$region" --filters "Name=vpc-id,Values=$vpc_id" "Name=map-public-ip-on-launch,Values=true" --query 'Subnets[0].AvailabilityZone' --output text)
-    echo "$az_public_subnet"
-}
-
 function createNodeGroups {
     ACTION=$1
     read -p "Are you sure you want to $ACTION NODES in the CLUSTER ${CLUSTER_NAME} in REGION ${AWS_REGION}? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
         ######################### GPU NODES #########################
-        # Read the YAML file and convert it to a JSON string
         instance_json=$(python -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=4)' <instance_list.yaml)
-        echo "nodegroup_type" >$CLUSTER_NAME-nodes.yaml
-        # Loop through the JSON array and populate the variables
+
         length=$(echo $instance_json | jq '.instances | length')
         for ((i = 0; i < $length; i++)); do
             export FAMILY=$(echo $instance_json | jq -r ".instances[$i].family")
-            INSTANCE_TYPE=$(echo $instance_json | jq -r ".instances[$i].instance_type")
             export SPOT_PRICE=$(echo $instance_json | jq -r ".instances[$i].spot_price")
-            nodegroup_type=$(echo $instance_json | jq -r ".instances[$i].nodegroup_type")
+            export NODEGROUP_TYPE=$(echo $instance_json | jq -r ".instances[$i].nodegroup_type")
+            export DESIRED_CAPACITY=$(echo $instance_json | jq -r ".instances[$i].desiredCapacity")
 
-            # Split instance_type into spot and ondemand
-            IFS=',' read -ra types <<<"$INSTANCE_TYPE"
-            for type in "${types[@]}"; do
+            echo "FAMILY: $FAMILY"
+            echo "SPOT_PRICE: $SPOT_PRICE"
+            echo "NODEGROUP_TYPE: $NODEGROUP_TYPE"
+            echo "DESIRED_CAPACITY: $DESIRED_CAPACITY"
 
-                # Check if instance family exist in the AZ
-                # if aws ec2 describe-instance-type-offerings \
-                #     --location-type availability-zone \
-                #     --filters Name=instance-type,Values=$FAMILY \
-                #     --query "InstanceTypeOfferings[?Location=='$AWS_AVAILABILITY_ZONE'].InstanceType" \
-                #     --output text | grep -q $FAMILY; then
+            # Create nodeGroups for the cluster
+            if [ "$ACTION" == "create" ]; then
+                envsubst <nodeGroups_gpu_$type.yaml | eksctl create nodegroup -f -
+            elif [ "$ACTION" == "delete" ]; then
+                envsubst <nodeGroups_gpu_$type.yaml | eksctl delete nodegroup --approve -f -
+            fi
 
-                export NODEGROUP_TYPE=$nodegroup_type-$type
-                echo "##################### $ACTION node: $FAMILY - $type"
-                echo "##########################################"
-                echo "Family:" $FAMILY
-                echo "Spot Price:" $SPOT_PRICE
-                echo "Nodegroup Type:" $NODEGROUP_TYPE
-                echo "- $NODEGROUP_TYPE" >>$CLUSTER_NAME-nodes.yaml
-
-                # Create nodeGroups for the cluster
-                if [ "$ACTION" == "create" ]; then
-                    envsubst <nodeGroups_gpu_$type.yaml | eksctl create nodegroup -f -
-                elif [ "$ACTION" == "delete" ]; then
-                    envsubst <nodeGroups_gpu_$type.yaml | eksctl delete nodegroup --approve -f -
-                fi
-                # else
-                #     echo "Instance type $FAMILY is NOT available in $AWS_AVAILABILITY_ZONE"
-                # fi
-            done
         done
         ######################### CPU NODES #########################
         if [ "$ACTION" == "create" ]; then
@@ -73,9 +46,6 @@ function createCluster {
         # Create ASG policy
         envsubst <policy.template.json >policy.json
         aws iam create-policy --policy-name ${AWS_POLICY_NAME} --policy-document file://policy.json
-
-        # Create CPU node
-        envsubst <nodeGroups_cpu.yaml | eksctl create nodegroup -f -
 
         # Get cluster credentials
         aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}
@@ -110,19 +80,21 @@ function deleteCluster {
 }
 
 ### Main
+export PREFIX_NAME="geocompas"
 export ENVIRONMENT=$1
 export AWS_REGION=$2
 export ACTION=$3
 export AWS_PARTITION="aws"
-export CLUSTER_NAME="devseed-k8s-${ENVIRONMENT}"
-export KUBERNETES_VERSION="1.27"
+export CLUSTER_NAME="${PREFIX_NAME}-k8s-${ENVIRONMENT}"
+export KUBERNETES_VERSION="1.29"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-export AWS_POLICY_NAME=devseed-k8s_${ENVIRONMENT}-${AWS_REGION}
+export AWS_POLICY_NAME=${PREFIX_NAME}-k8s_${ENVIRONMENT}-${AWS_REGION}
 export AWS_POLICY_ARN=arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${AWS_POLICY_NAME}
-export VPC_NAME="eksctl-${CLUSTER_NAME}-cluster/VPC"
-# export AWS_AVAILABILITY_ZONE=us-east-1f
-export AWS_AVAILABILITY_ZONE=$(get_az_public_subnet $VPC_NAME $AWS_REGION)
+
+export AWS_AVAILABILITY_ZONE="us-east-1a"
+
 echo "Make sure you have created a Key pairs, called: k8s-sam-${AWS_REGION}..."
+
 ACTION=${ACTION:-default}
 if [ "$ACTION" == "create_cluster" ]; then
     createCluster
