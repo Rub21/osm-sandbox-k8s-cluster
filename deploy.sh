ce819c0ddf606d2faae8341241aa9f052cb8c627
Rub21 <rub2106@gmail.com>
2024-06-08 15:17:27 -0500

    Update config

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
-
+mkdir -p tmp
 function createNodeGroups {
     ACTION=$1
     read -p "Are you sure you want to $ACTION NODES in the CLUSTER ${CLUSTER_NAME} in REGION ${AWS_REGION}? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
         ######################### GPU NODES #########################
-        instance_json=$(python -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=4)' <instance_list.yaml)
+        instance_json=$(python3 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=4)' <instance_list.yaml)
 
         length=$(echo $instance_json | jq '.instances | length')
         for ((i = 0; i < $length; i++)); do
@@ -21,19 +21,13 @@ function createNodeGroups {
             echo "DESIRED_CAPACITY: $DESIRED_CAPACITY"
 
             # Create nodeGroups for the cluster
+            # envsubst <nodeGroups.yaml > tmp/nodeGroups.yaml
             if [ "$ACTION" == "create" ]; then
-                envsubst <nodeGroups_gpu_$type.yaml | eksctl create nodegroup -f -
+                envsubst <nodeGroups.yaml | eksctl create nodegroup -f -
             elif [ "$ACTION" == "delete" ]; then
-                envsubst <nodeGroups_gpu_$type.yaml | eksctl delete nodegroup --approve -f -
+                envsubst <nodeGroups.yaml | eksctl delete nodegroup --approve -f -
             fi
-
         done
-        ######################### CPU NODES #########################
-        if [ "$ACTION" == "create" ]; then
-            envsubst <nodeGroups_cpu_workers.yaml | eksctl create nodegroup -f -
-        elif [ "$ACTION" == "delete" ]; then
-            envsubst <nodeGroups_cpu_workers.yaml | eksctl delete nodegroup --approve -f -
-        fi
     fi
 }
 
@@ -41,32 +35,51 @@ function createCluster {
     read -p "Are you sure you want to CREATE a CLUSTER ${CLUSTER_NAME} in REGION ${AWS_REGION}? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
         # Create cluster
-        envsubst <cluster.yaml | eksctl create cluster -f -
+        # envsubst <cluster.yaml | eksctl create cluster -f -
 
         # Create ASG policy
-        envsubst <policy.template.json >policy.json
-        aws iam create-policy --policy-name ${AWS_POLICY_NAME} --policy-document file://policy.json
+        envsubst <policy.template.json > tmp/policy.json
+        aws iam create-policy --policy-name ${AWS_POLICY_NAME} --policy-document file://tmp/policy.json
 
-        # Get cluster credentials
-        aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}
-        kubectl cluster-info
+        # # Get cluster credentials
+        # aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}
+        # kubectl cluster-info
 
-        # Install eb-csi addons
-        kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master"
-        kubectl get pods -n kube-system | grep ebs-csi
+        # # Install eb-csi addons
+        # kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master"
+        # kubectl get pods -n kube-system | grep ebs-csi
 
-        # Metricts server
-        kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-        kubectl get pods -n kube-system | grep metrics-server
+        # # Metricts server
+        # kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+        # kubectl get pods -n kube-system | grep metrics-server
 
-        # Install autoscaler
-        envsubst <asg-autodiscover.yaml | kubectl apply -f -
-        kubectl get pods --namespace=kube-system | grep autoscaler
+        # # Install autoscaler
+        # envsubst <asg-autodiscover.yaml | kubectl apply -f -
+        # kubectl get pods --namespace=kube-system | grep autoscaler
 
-        # Update aws-auth
-        kubectl get configmap aws-auth -n kube-system -o yaml >aws-auth.yaml
-        echo "Update manually aws-auth.yaml, use as example mapUsers.yaml"
-        echo "kubectl apply -f aws-auth.yaml"
+        # Instaling ingress-nginx and cert-manager
+
+        # ### Instaling ingress-nginx and cert-manager
+        # helm repo add jetstack https://charts.jetstack.io
+        # helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+        # helm repo update
+
+        # helm install \
+        # cert-manager jetstack/cert-manager \
+        # --namespace cert-manager \
+        # --create-namespace \
+        # --version v1.15.0 \
+        # --set crds.enabled=true
+        
+        # helm install nginx-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+        # kubectl get pods -n cert-manager
+        # kubectl get pods -n ingress-nginx
+
+  
+        # # # Update aws-auth
+        # kubectl get configmap aws-auth -n kube-system -o yaml >aws-auth.yaml
+        # echo "Update manually aws-auth.yaml, use as example mapUsers.yaml"
+        # echo "kubectl apply -f aws-auth.yaml"
     fi
 
 }
@@ -80,20 +93,18 @@ function deleteCluster {
 }
 
 ### Main
-export PREFIX_NAME="geocompas"
 export ENVIRONMENT=$1
-export AWS_REGION=$2
-export ACTION=$3
+export ACTION=$2
+
+export AWS_REGION="us-east-1"
+export AWS_AVAILABILITY_ZONE="us-east-1a"
+export PREFIX_NAME="osm-us"
 export AWS_PARTITION="aws"
 export CLUSTER_NAME="${PREFIX_NAME}-k8s-${ENVIRONMENT}"
 export KUBERNETES_VERSION="1.29"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 export AWS_POLICY_NAME=${PREFIX_NAME}-k8s_${ENVIRONMENT}-${AWS_REGION}
 export AWS_POLICY_ARN=arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${AWS_POLICY_NAME}
-
-export AWS_AVAILABILITY_ZONE="us-east-1a"
-
-echo "Make sure you have created a Key pairs, called: k8s-sam-${AWS_REGION}..."
 
 ACTION=${ACTION:-default}
 if [ "$ACTION" == "create_cluster" ]; then
